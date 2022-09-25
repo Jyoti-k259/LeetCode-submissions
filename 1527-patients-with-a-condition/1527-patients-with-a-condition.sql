bba8d76b0bfc09a236208793a1d54fed5962f09a
Jyoti-k259 <57934790+Jyoti-k259@users.noreply.github.com>
2022-09-25 18:17:44 -0400

    Time: 347 ms (68.23%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
+# 'DIAB1%' this checks for initial position in a word and '% DIAB1%' has a space in the start ,hence checks the second word
 
 select  * from patients where Conditions LIKE 'DIAB1%'or Conditions LIKE '% DIAB1%' order by patient_id ;
